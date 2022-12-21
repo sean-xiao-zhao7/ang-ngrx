a2df929cf456a37cf2e162711f9e56b60d4dc2d2
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-12-21 18:47:20 -0500

    Add addIngredient action/reducer

---
+++ src/app/shopping-list/shopping-list-store/shopping-list.reducer.ts
@@ -0,0 +1,19 @@
+import { Ingredient } from "../../shared/ingredient.model";
+import * as actions from "./shopping-list.actions";
+
+const initialState = {
+  ingredients: [new Ingredient("Apples", 5), new Ingredient("Tomatoes", 10)],
+};
+
+export function shoppingListReducer(
+  state = initialState,
+  action: actions.AddIngredientAction
+) {
+  switch (action.type) {
+    case actions.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload],
+      };
+  }
+}
