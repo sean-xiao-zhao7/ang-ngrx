5bb8c5464578e7ee85600087f4ed12f21defbcb1
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-12-23 18:29:07 -0500

    Use shopping list reducer

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,40 +1,33 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Observable } from "rxjs";
 
-import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from './shopping-list.service';
-import { LoggingService } from '../logging.service';
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "./shopping-list.service";
+import { LoggingService } from "../logging.service";
+import { Store } from "@ngrx/store";
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  selector: "app-shopping-list",
+  templateUrl: "./shopping-list.component.html",
+  styleUrls: ["./shopping-list.component.css"],
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[];
-  private subscription: Subscription;
+  ingredients: Observable<{ ingredients: Ingredient[] }>;
 
   constructor(
     private slService: ShoppingListService,
-    private loggingService: LoggingService
+    private loggingService: LoggingService,
+    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
   ) {}
 
   ngOnInit() {
-    this.ingredients = this.slService.getIngredients();
-    this.subscription = this.slService.ingredientsChanged.subscribe(
-      (ingredients: Ingredient[]) => {
-        this.ingredients = ingredients;
-      }
-    );
-
-    this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit!');
+    this.ingredients = this.store.select("shoppingList");
+    this.loggingService.printLog("Hello from ShoppingListComponent ngOnInit!");
   }
 
   onEditItem(index: number) {
     this.slService.startedEditing.next(index);
   }
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
+  ngOnDestroy() {}
 }
